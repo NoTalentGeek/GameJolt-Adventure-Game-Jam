ce32acc4b8b6e236b31c5a370ffc10598e558737
notalentgeek <mikael.pratama@yahoo.com>
2016-05-10 11:33:13 +0200

    Before changing the map resolution.

--- src/state/room/_00MainRoomState.js
+++ src/state/room/_00MainRoomState.js
@@ -4,11 +4,14 @@ var _00MainRoomState = {
 
         console.log("_00MainRoomState.js.");
 
-        _Game.camera.scale.x = 4;
-        _Game.camera.scale.y = 4;
+        //_Game.camera.scale.x = 4;
+        //_Game.camera.scale.y = 4;
+        _Game.world.setBounds(0, 0, 2048, 1152);
         _Game.physics.arcade.gravity.y = 1200;
 
         this._Tilemap = _Game.add.tilemap("_00MainRoomPath_Tilemap");
+        console.log(this._Tilemap.scale);
+        this._Tilemap.scale = { x: 2, y: 2 };
         this._Tilemap.addTilesetImage("Prototype32X32MainCharacter", "Prototype32X32MainCharacter_Image");
         this._Tilemap.addTilesetImage("Prototype96X96Boulder", "Prototype96X96Boulder_Image");
         this._Tilemap.addTilesetImage("Prototype96X96MainTile", "Prototype96X96MainTile_Image");
@@ -22,10 +25,10 @@ var _00MainRoomState = {
     },
     update:function(){
 
+        //_Game.camera.y += 1;
         //_Game.camera.y = 576 - 144;
-        _Game.camera.y += 1;
-        ControlMainCameraBound_Void();
-        console.log(_Game.camera.y);
+        //console.log(_Game.camera.y);
+        //ControlMainCameraBound_Void();
 
     },
     CreateSprite_Void:function(
